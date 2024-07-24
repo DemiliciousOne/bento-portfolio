68310c899bc7dd1341727016aabff26a8335532a
Demi Yang <verussapientia@gmail.com>
2024-07-24 12:02:39 -0400

    test if icon works

--- src/utils/portfolioData.ts
+++ src/utils/portfolioData.ts
@@ -17,7 +17,7 @@ export const portfolioData = [
     description:
       "A modern remote job board that shows hundreds of new opportunities each day.",
     featureImage: "/screenshots/career-vault-screenshot.png",
-    achievementIcon: "/src/assets/icons/people.svg",
+    achievementIcon: "/icons/people.svg",
     achievement: "73k monthly visitors",
     techStack: ["SolidJS", "Astro", "TypeScript", "Node.js"],
   },
@@ -30,25 +30,4 @@ export const portfolioData = [
     featureImage: "/screenshots/rpm-screenshot.png",
     techStack: ["Webflow", "Figma", "JavaScript"],
   },
-  // {
-  //   id: 3,
-  //   title: "Caya",
-  //   url: "https://magazin.getcaya.com/",
-  //   description:
-  //     "(Improved and maintained) blog and marketing website for a document management SaaS.",
-  //   featureImage: "/screenshots/caya-screenshot.png",
-  //   techStack: ["React", "TypeScript", "GraphQL", "Ghost"],
-  // },
-  // {
-  //   id: 4,
-  //   title: "Cosmo",
-  //   url: "https://hirecosmo.com",
-  //   description:
-  //     "Marketplace connecting companies with top developers worldwide.",
-  //   featureImage: "/screenshots/cosmo-screenshot.png",
-  //   achievementIcon: "/src/assets/icons/under-construction.svg",
-  //   achievement: "In progress",
-  //   techStack: ["Webflow", "Figma", "JavaScript"],
-  // },
-  // Add more project objects for each portfolio project
 ];
